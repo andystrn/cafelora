4ed8ab03a3f1af7c0f48160bcd29f3ccac033ddf
Andrea Strnadlova <an.strnadlova@gmail.com>
2022-12-09 15:36:55 +0100

    content divided to components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './style.css';
+import { Header } from './components/Header/index.js';
+import { Banner } from './components/Banner/index.js';
+import { Menu } from './components/Menu/index';
+import { Gallery } from './components/Gallery/index.js';
+import { Contact } from './components/Contact/index.js';
+import { Footer } from './components/Footer/index.js';
 
-console.log('funguju!');
+const pageElement = document.createElement('div');
+pageElement.classList.add('page');
+
+const main = document.createElement('main');
+main.append(Banner(), Menu(),Gallery(),Contact());
+
+pageElement.append(Header(), main, Footer());
+
+document.querySelector('#app').append(pageElement);
